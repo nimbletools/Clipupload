98aa4a08a1537dd5d2af0bf2b09805f94f82489c
Angelo Geels <spansjh@gmail.com>
2015-01-09 14:11:38 +0100

    Resized the authentication dialog

--- Facebook/FormAuthenticate.Designer.cs
+++ Facebook/FormAuthenticate.Designer.cs
@@ -23,41 +23,41 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAuthenticate));
-            this.web = new System.Windows.Forms.WebBrowser();
-            this.timerWait = new System.Windows.Forms.Timer(this.components);
-            this.SuspendLayout();
-            // 
-            // web
-            // 
-            this.web.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.web.Location = new System.Drawing.Point(0, 0);
-            this.web.MinimumSize = new System.Drawing.Size(20, 20);
-            this.web.Name = "web";
-            this.web.Size = new System.Drawing.Size(775, 434);
-            this.web.TabIndex = 0;
-            // 
-            // timerWait
-            // 
-            this.timerWait.Enabled = true;
-            this.timerWait.Interval = 1000;
-            this.timerWait.Tick += new System.EventHandler(this.timerWait_Tick);
-            // 
-            // FormAuthenticate
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 434);
-            this.Controls.Add(this.web);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "FormAuthenticate";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Authenticate Facebook";
-            this.ResumeLayout(false);
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAuthenticate));
+      this.web = new System.Windows.Forms.WebBrowser();
+      this.timerWait = new System.Windows.Forms.Timer(this.components);
+      this.SuspendLayout();
+      // 
+      // web
+      // 
+      this.web.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.web.Location = new System.Drawing.Point(0, 0);
+      this.web.MinimumSize = new System.Drawing.Size(20, 20);
+      this.web.Name = "web";
+      this.web.Size = new System.Drawing.Size(535, 547);
+      this.web.TabIndex = 0;
+      // 
+      // timerWait
+      // 
+      this.timerWait.Enabled = true;
+      this.timerWait.Interval = 1000;
+      this.timerWait.Tick += new System.EventHandler(this.timerWait_Tick);
+      // 
+      // FormAuthenticate
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(535, 547);
+      this.Controls.Add(this.web);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "FormAuthenticate";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Authenticate Facebook";
+      this.ResumeLayout(false);
 
         }
 
